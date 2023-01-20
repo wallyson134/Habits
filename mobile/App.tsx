6ab6c76dc1713da09b0cfbf1c68f2587d4be2691
wallyson134 <fwnsilva@aluno.uespi.br>
2023-01-20 00:02:01 -0300

    Avançando no Front,no mobile e web,e novas rotas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,16 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native'
-import {
+import './src/lib/dayjs';
+
+import { StatusBar } from 'react-native';
+import { 
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_800ExtraBold
-} from '@expo-google-fonts/inter'
-import { Loading } from './src/components/Loading'
+} from '@expo-google-fonts/inter';
+
+import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -14,35 +18,18 @@ export default function App() {
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_800ExtraBold
-  })
+  });
 
   if (!fontsLoaded) {
-    return <Loading />
+    return (
+      <Loading />
+    );
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-    </View>
-  )
+    <>
+      <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    color: '#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-})
--- mobile/src/components/Loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ActivityIndicator, View } from 'react-native'
-
-export function Loading() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <ActivityIndicator color="#7C3AED"></ActivityIndicator>
-    </View>
-  )
-}
